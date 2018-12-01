c7626a4d8efddf01a3bca1120a5cbb6ca6e6b9ff
fel <fen1499@gmail.com>
2018-12-01 10:05:10 -0200

    Construindo o profview, adicionar requisição AJAX no form

--- _view/profview.php
+++ _view/profview.php
@@ -4,12 +4,22 @@
     <li><button onclick=showhidden() >ALTERAR HORARIO</button></li>
 </ul>
 <div id="seila" hidden>
-    AAAA
+    <p>Disciplina <input name="disc"></p>
+    <p>Professor <input name="prof"></p>
+    <p>Sala <input name="sala"></p>
+    <p id="horap">Horarios <input name="hora" min="7" max="21" size="2">
+    <button onclick="addhora()">+</button></p>
+    <p><input value="Add"></p>
 </div>
 <script>
     function showhidden()
     {
         x = document.getElementById("seila");
         x.hidden = !x.hidden;
+    }
+
+    function addhora()
+    {
+        document.getElemenyById("horap").write(<input name="hora" min="7" max="21" size="2">);
     }
 </script>
